e96090c3b6607f3416fc671a32373d9376706d38
Meghan Hein <meghan.hein@gmail.com>
2020-10-25 16:53:13 -0700

    add components to root

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AppProvider from '../context/AppProvider'
+import Header from './Header'
+import Toolbar from './toolbar'
+import Canvas from './Canvas'
+import Imports from './toolbar/Imports'
+import Footer from './Footer'
+import './App.scss';
 
-function App() {
+export default function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppProvider>
+        <Header/>
+        <Toolbar/>
+        <Canvas/>
+        <Imports/>
+        <Footer/>
+      </AppProvider>
     </div>
   );
 }
-
-export default App;
