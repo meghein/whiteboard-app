79e101454ea512507ecb97ae7397092ccba3ac96
Meghan Hein <meghan.hein@gmail.com>
2020-10-26 18:51:24 -0700

    remove leftover code from hook

--- src/components/Toolbar/Imports.js
+++ src/components/Toolbar/Imports.js
@@ -1,25 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { useStateContext, useDispatchContext } from 'context/Provider';
 import { ACTIONS } from 'reducer/app'
-// import useDragDrop from 'hooks/useDragDrop';
 import './style.scss';
 
 export default function Imports() {
   const state = useStateContext();
   const dispatch = useDispatchContext();
-  // const { onDragStart } = useDragDrop();
   const [toggle, setToggle] = useState(false)
-  
-  function toggleButton() {
-    toggle ? setToggle(false) : setToggle(true)
-  }
   
   // open imports when a new file is uploaded
   useEffect(() => {
     setToggle(false)
   }, [state.imports])
 
-  function handleImageClick(e) {
+  function toggleButton() {
+    toggle ? setToggle(false) : setToggle(true)
+  }
+  
+  function handleClick(e) {
     dispatch({type: ACTIONS.IMAGES, payload: e.target.src})
   }
 
@@ -40,7 +38,7 @@ export default function Imports() {
               src={item.image}
               alt={item.id}
               key={index}
-              onClick={handleImageClick}
+              onClick={handleClick}
               onDragStart={handleDrag}
             />
         )})
